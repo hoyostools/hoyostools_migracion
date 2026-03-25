45649b7b2165739554d0d93e713b429c833ddf22
estivencastro <ingenieria2@hoyostools.com>
2026-03-25 17:02:33 -0500

    FIX

--- auto_inventory_location/models/product_template.py
+++ auto_inventory_location/models/product_template.py
@@ -4,14 +4,15 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    def write(self, vals):
-        res = super().write(vals)
-        if 'ubicacion_reab' in vals:
-            self._onchange_reab()
+    def _compute_ubicacion_reab(self):
+        res = super()._compute_ubicacion_reab()
+        for record in self:
+            record._onchange_reab
         return res
 
     @api.onchange('ubicacion_reab')
     @api.depends('ubicacion_reab')
+    @api.constrains('ubicacion_reab')
     def _onchange_reab(self):
         product = self.env['product.product'].search([('product_tmpl_id', '=', self.product_variant_id.id)])
         if not self.ubicacion_reab or self.ubicacion_reab.complete_name != 'CLH/Existencias/U05/Pasillo 01/Sin Regla Abastecer U05':
