c2dd1a8b1aff3bff3520946f62c6c0d602eeefd4
estivencastro <ingenieria2@hoyostools.com>
2026-03-25 17:11:57 -0500

    FIX

--- auto_inventory_location/models/product_template.py
+++ auto_inventory_location/models/product_template.py
@@ -15,7 +15,7 @@ class ProductTemplate(models.Model):
     @api.constrains('ubicacion_reab')
     def _onchange_reab(self):
         product = self.env['product.product'].search([('product_tmpl_id', '=', self.product_variant_id.id)])
-        if not self.ubicacion_reab or self.ubicacion_reab.complete_name != 'CLH/Existencias/U05/Pasillo 01/Sin Regla Abastecer U05':
+        if self.ubicacion_reab and self.ubicacion_reab.complete_name != 'CLH/Existencias/U05/Pasillo 01/Sin Regla Abastecer U05':
             records = self.env["auto.location.record"].search([
                 ("product_id", "=", product.id),
                 ("sin_regla", "=", True)
