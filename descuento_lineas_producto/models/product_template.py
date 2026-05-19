3166f787434b4a326568d32a4fb82960ef0a8115
Hoyostools <soporte@hoyostools.com>
2026-05-19 11:23:28 -0500

    ADD actualizar etiquetas de campos en el modelo de producto

--- descuento_lineas_producto/models/product_template.py
+++ descuento_lineas_producto/models/product_template.py
@@ -3,12 +3,12 @@ from odoo import fields, models, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    valor_a = fields.Float(string='Cantidad máxima para descuento:', default=False)
-    valor_b = fields.Float(string='Cantidad minima para descuento:', default=False)
-    descuento_rango = fields.Integer(string='Descuento en rango:', default=False)
-    descuento_mayor = fields.Integer(string='Descuento supera rango:', default=False)
-    applicable_sale_order = fields.Boolean(string="Venta")
-    applicable_web = fields.Boolean(string="Sitio Web")
+    valor_a = fields.Float(string='Hasta (Unidades)', default=False)
+    valor_b = fields.Float(string='De (Unidades)', default=False)
+    descuento_rango = fields.Integer(string='Obtienes % Descuento', default=False)
+    descuento_mayor = fields.Integer(string='Si es Mayor Obtienes % Descuento', default=False)
+    applicable_sale_order = fields.Boolean(string="Aplicable a Ventas")
+    applicable_web = fields.Boolean(string="Aplicable en Web")
 
 
 
