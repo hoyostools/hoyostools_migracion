7f5ffe87d741a52ed75c4d4b081dd2ac6748ab10
estivencastro <ingenieria2@hoyostools.com>
2026-05-21 14:21:06 -0500

    Arreglo en vista, eliminar campo inexistente.

--- customer_journal/models/res_users.py
+++ customer_journal/models/res_users.py
@@ -4,4 +4,4 @@ from odoo import _, api, models, fields
 class ResUser(models.Model):
     _inherit = "res.users"
 
-    account_journal_id = fields.Many2many('account.journal', string="Diarios habilitados")
+    account_journal_ids = fields.Many2many('account.journal', string="Diarios habilitados")
